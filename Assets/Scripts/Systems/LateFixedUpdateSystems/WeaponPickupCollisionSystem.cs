f2ee92ba682f38b5c467275f74503dd55392905c
aleksandr.cherkasov <aleksandr.cherkasov@thetoadman.com>
2022-10-19 10:45:59 +0300

    spawning shotgun in players hands on pickup

--- Assets/Scripts/Systems/LateFixedUpdateSystems/WeaponPickupCollisionSystem.cs
+++ Assets/Scripts/Systems/LateFixedUpdateSystems/WeaponPickupCollisionSystem.cs
@@ -30,9 +30,36 @@ public class WeaponPickupCollisionSystem : EcsSystem
                 var attackReach = world.Have<ReachComponent>(id) ? world.GetComponent<ReachComponent>(id).distance : float.PositiveInfinity;
                 world.GetOrAddComponentRef<AttackReachComponent>(collidedId).distance = attackReach;
                 world.GetOrAddComponentRef<Owner>(id).entity = collidedEntity;
-                Object.Destroy(world.GetComponent<Transform>(id).gameObject);
+
+                var weaponTransform = world.GetComponent<Transform>(id);
+                var weaponCollider = weaponTransform.gameObject.GetComponent<Collider>();
+                if (weaponCollider != null)
+                    weaponCollider.enabled = false;
+
+                var playerTransform = world.GetComponent<Transform>(collidedId);
+                var GunHolder = FindGrandChildByName(playerTransform, "GunHolder");
+                var GunGrip = FindGrandChildByName(weaponTransform, "Grip");
+
+                weaponTransform.SetParent(GunHolder);
+                weaponTransform.localPosition = -GunGrip.localPosition;
+                weaponTransform.localRotation = Quaternion.identity;
+
                 world.Remove<Transform>(id);
             }
         }
+    }
+
+    private Transform FindGrandChildByName(Transform transform, string name)
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.name == name)
+                return child;
+            var childTransform = FindGrandChildByName(child, name);
+            if (childTransform != null)
+                return childTransform;
+        }
+
+        return null;
     }
 }
