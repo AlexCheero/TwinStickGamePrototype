e8434729c7bcf57ffc7a3468b87fd980ae74792e
alexcheero <alexcheero157@gmail.com>
2022-11-04 22:25:55 +0300

    score counter

--- Assets/Scripts/Monobehaviours/MainMenu.cs
+++ Assets/Scripts/Monobehaviours/MainMenu.cs
@@ -4,11 +4,18 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    private const string SCORE_KEY = "Score";
+    
     [SerializeField]
     private Button _startNewGame;
+
+    [SerializeField]
+    private Text _score;
     
     void Start()
     {
+        int score = PlayerPrefs.HasKey(SCORE_KEY) ? PlayerPrefs.GetInt(SCORE_KEY) : 0;
+        _score.text = "Score: " + score;
         _startNewGame.onClick.AddListener(OnStartNewGameClicked);
     }
 
